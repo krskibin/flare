dc035e7645203beb6a071d0004ae7c8b7abfc265
krskibin <chrisski96@gmail.com>
2018-03-02 22:40:10 +0100

    Initial Commit

---
+++ pam/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  pam
+//
+//  Created by Krystian Skibiński on 02.03.2018.
+//  Copyright © 2018 Krystian Skibiński. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
