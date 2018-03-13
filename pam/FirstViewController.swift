eb9456b071ae43c86a943881dcee730de8813631
tdobrowolski <tobdob15@gmail.com>
2018-03-13 10:28:44 +0100

    Add TableView. #1b1uk

--- pam/FirstViewController.swift
+++ pam/FirstViewController.swift
@@ -1,12 +1,34 @@
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let elements = ["Apple się kończy", "SGS9 jest super", "Xiaomi podbija Polskę"]
+    
+    @IBOutlet weak var newsTableView: UITableView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true // duzy NavBar
+        
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return elements.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 105
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = newsTableView.dequeueReusableCell(withIdentifier: "customCell") as! NewsTableViewCell
+        cell.newsLabel.text = elements[indexPath.row]
+        cell.newsImage.image = UIImage(named: "image")
+        cell.newsImage.layer.cornerRadius = 8
+        return cell
     }
     
     func isNumberEven(number: Int) -> Bool {
---
+++ pam/NewsTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  NewsTableViewCell.swift
+//  pam
+//
+//  Created by Tobiasz Dobrowolski on 13.03.2018.
+//  Copyright © 2018 Krystian Skibiński. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var newsImage: UIImageView!
+    @IBOutlet weak var newsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
