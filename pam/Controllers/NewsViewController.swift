ead7cb660ee05cfe3ecde0bdf73c97813df33d41
tdobrowolski <tobdob15@gmail.com>
2018-06-07 21:37:52 +0200

    ZEPSUTE
    zepsute

--- pam/Controllers/NewsViewController.swift
+++ pam/Controllers/NewsViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class NewsViewController: UIViewController {
+class NewsViewController: UIViewController, UIScrollViewDelegate {
     var pressedTitle: String?
     var pressedLink: String?
     var pressedImage: String?
@@ -10,9 +10,13 @@ class NewsViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UITextView!
     @IBOutlet weak var linkLabel: UILabel!
     @IBOutlet weak var topImage: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var titleTextView: UITextView!
+    @IBOutlet weak var descriptionTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad")
         
         let imageUrl = URL(string: pressedImage!)
         let data = try? Data(contentsOf: imageUrl!)
@@ -24,14 +28,35 @@ class NewsViewController: UIViewController {
         self.navigationController?.navigationBar.isTranslucent = true
         self.navigationController!.view.backgroundColor = UIColor.clear
         self.navigationController?.navigationBar.backgroundColor = UIColor.clear
+        setTransluscentNavBar()
+        
+        self.scrollView.delegate = self
+        
+        descriptionTextView.sizeToFit()
+        descriptionTextView.isScrollEnabled = false
 
-        titleLabel.text = pressedTitle!
+        titleTextView.text = pressedTitle!
         linkLabel.text = pressedLink!
-        descriptionLabel.text = pressedDescription!
+        descriptionTextView.text = pressedDescription!
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
+    }
+    
+    func setTransluscentNavBar() {
+        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController!.view.backgroundColor = UIColor.clear
+        self.navigationController?.navigationBar.backgroundColor = UIColor.clear
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        self.navigationController!.navigationBar.isTranslucent = false
+    }
+    
+    func scrollViewDidScrollToTop(_ scrollView: UIScrollView) {
+        setTransluscentNavBar()
     }
 }
